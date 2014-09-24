9f3318de3e918a89db73ca21b29073d681036d18
Bojan Nikolic <bojan@bnikolic.co.uk>
2014-09-24 22:13:33 +0100

    Add license to the splash blurb

--- ExPy/ExPy.py
+++ ExPy/ExPy.py
@@ -9,6 +9,8 @@ import types
 import ctypes
 import xlltypes
 
+__version__ = 0.4
+
 # Holds ancillary data about all registred functions
 fnregstry = {}
 
@@ -17,21 +19,6 @@ fnregstry = {}
 # problems. See github issue #2
 reslist = []
 
-def Splash():
-    title="BN Algortihms Excel/Python add-in "
-    blurb= """
-    This is the Python add-in for Excel 
-
-    Version: 0.3
-
-    Visit us at: http://www.bnikolic.co.uk/expy
-    Inquiries/bug reports to: webs@bnikolic.co.uk
-
-    """
-    ctypes.windll.user32.MessageBoxA(0,
-                                     blurb, 
-                                     title, 
-                                     0x40)
 
 def nArgs(formula):
     """
@@ -120,3 +107,51 @@ def xlStack():
          None)
     return rr
 
+
+def Splash():
+    title="BN Algortihms Excel/Python add-in "
+    blurb= """
+    This is the ExPy Python add-in for Microsoft Excel 
+
+    Version: %g
+
+    Visit us at: http://www.bnikolic.co.uk/expy
+    Inquiries/bug reports to: webs@bnikolic.co.uk
+
+    """% (__version__,)
+    lic="""
+Copyright (C) Copyright (c) Bojan Nikolic <bojan@bnikolic.co.uk> 2010, 2013, 2014
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+    * Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above
+       copyright notice, this list of conditions and the following
+       disclaimer in the documentation and/or other materials provided
+       with the distribution.
+
+    * Neither the name of the ExPy Developers nor the names of any
+       contributors may be used to endorse or promote products derived
+       from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    
+    """
+    ctypes.windll.user32.MessageBoxA(0,
+                                     blurb+lic, 
+                                     title, 
+                                     0x40)
